11cdbca6feeafd7bc6e875404698f0160d1584fe
aizj <aizj@marvoto.com>
2019-08-12 14:54:26 +0800

    get envelop for single line of US

--- ultraSonorFrontProcess.cpp
+++ ultraSonorFrontProcess.cpp
@@ -20,6 +20,7 @@
 #define  MIN_CANDIDAT_DIAMETER    (200)
 #define  MIN_CANDIDAT_AREA        (1000)
 #define  POINT_NUM        2584
+#define  DOWN_RATE      8
 vector<vector<Point> > g_Contours;
 vector<strAreaTag> g_AllArea,g_CandidateArea;
 vector<strAreaTag> * pAreaTag = &g_AllArea;
@@ -149,7 +150,7 @@ int CalcDist( vector<strAreaTag> * pData, int w, int h)
 
 int main()
 {
-    int i,j,iRet = 0;
+    int i,j,k,iRet = 0;
     int mc_valid[300] = {1};
     RNG g_rng(12345);
     Mat g_cannyMat_output;
@@ -160,6 +161,7 @@ int main()
     
 	Mat m_line = Mat(POINT_NUM,1,CV_16SC1,iDataLine);
 	Mat m_lineEnvelop = Mat(POINT_NUM,1,CV_16SC1);
+	Mat m_pryDown = Mat(POINT_NUM/DOWN_RATE,1,CV_16SC1);
 	//memcpy(g_line.data,(char*)&iDataLine,2584*2);  // int64 maybe not ok
 	printf("[0]=%d,[1000]=%d,[2000]=%d,[2583]=%d\n", m_line.at<short>(0,0),m_line.at<short>(1000,0),m_line.at<short>(2000,0),m_line.at<short>(2583,0));
 	//printf("[0]=%d,[100]\n", g_line.at<short>(1000,0));
@@ -199,7 +201,7 @@ int main()
 	}
 	absFile.close();
 	
-	//s3 get envelop
+	//s3 get envelop and normalize
     fstream envFile("D:/prj/z1/src/CvCounter/3envelop.txt",ios::out|ios::trunc);
 	if(!envFile.is_open()){
 	    printf("error: can not open 3envelop.txt\n");
@@ -209,8 +211,7 @@ int main()
 	memset(str,0,sizeof(str));
 	short t1,t2;
 	m_envlop->env_half((short *)m_line.data,(short *)m_lineEnvelop.data,POINT_NUM);
-
-	//s4 blur and save
+	cv::normalize(m_lineEnvelop,m_lineEnvelop,0,255,cv::NORM_MINMAX);
 	GaussianBlur(m_lineEnvelop, m_lineEnvelop, Size(15, 15), 1, 1);
 	for( i=0; i<height ;i++){
 		for( j=0; j<width; j++){
@@ -222,6 +223,32 @@ int main()
 		}
 	}
 	envFile.close();
+
+	//s4 prydown4
+	fstream downFile("D:/prj/z1/src/CvCounter/4downFile.txt",ios::out|ios::trunc);
+	if(!downFile.is_open()){
+	    printf("error: can not open downFile.txt\n");
+		downFile.close();
+		return -2;
+	}
+	memset(str,0,sizeof(str));
+	//cv::pyrDown(m_lineEnvelop,m_pryDown,cv::Size(m_pryDown.cols/4,m_pryDown.rows));	
+	for( i=0; i<m_lineEnvelop.cols;i++){
+		for( j=0; j<m_lineEnvelop.rows; j+=DOWN_RATE){
+			if(j>=DOWN_RATE){
+				iRet = 0;
+				for(k=0;k<DOWN_RATE;k++){
+				    iRet += m_lineEnvelop.at<short>(j-k,i);
+				}
+				m_pryDown.at<short>(j/DOWN_RATE,i) = iRet/DOWN_RATE;
+			}else if(0==i && 0==j){
+				m_pryDown.at<short>(0,0) = m_lineEnvelop.at<short>(0,0);
+			}else{;}
+			sprintf(str,"%d,",m_pryDown.at<short>(j/DOWN_RATE,i));
+			downFile.write(str, strlen(str) );
+		}
+	}
+	downFile.close();
 	waitKey();
     return 0;
 }
